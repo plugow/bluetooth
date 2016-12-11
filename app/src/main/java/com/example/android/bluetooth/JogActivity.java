8ca46d2bd44b829c702eff10394bed558618553d
plugow <edypus@gmail.com>
2016-12-12 00:16:44 +0100

    jogi

--- app/src/main/java/com/example/android/bluetooth/JogActivity.java
+++ app/src/main/java/com/example/android/bluetooth/JogActivity.java
@@ -3,17 +3,56 @@ package com.example.android.bluetooth;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class JogActivity extends AppCompatActivity {
+    int velocity=1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.jog_item);
+
+        View decorView = getWindow().getDecorView();
+        int uiOption = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                |View.SYSTEM_UI_FLAG_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        decorView.setSystemUiVisibility(uiOption);
+
+
+        // checking value of seekbar
+        SeekBar velocityBar=(SeekBar) findViewById(R.id.seekBar);
+        final TextView velocityTextView=(TextView) findViewById(R.id.velocityTextView);
+        velocityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                velocity=i+1;
+                if (i==100) velocity=100;
+                velocityTextView.setText("Velocity: "+velocity+"%");
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
 
     }
+
+
 }
